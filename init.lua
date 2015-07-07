394a652e9092f7af513a345eb5d32950f72628e7
Jon Allen <ylixir@gmail.com>
2015-07-07 06:07:12 -0500

    started implementing control stuff

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local mode
+local mode, modes
 
 local function update_ui()
 	ui.statusbar_text = mode..' mode'
@@ -18,37 +18,74 @@ local function insert_mode()
 	update_ui()
 end
 
-local command = {
-	['h'] = buffer.char_left,
-	['j'] = buffer.line_down,
-	['k'] = buffer.line_up,
-	['l'] = buffer.char_right,
-	['i'] = insert_mode,
-}
-
+--catchall
 keys['esc'] = command_mode
+
+--debug reminder thingie
+function unimplemented() print('unimplemented') end
+
+--start in command mode by default
+command_mode()
+
 
 --[[the mode stuff built into the ta api allows unbound letters through
 	but filters out things like ctrl-w and ctrl-s which is basically exactly
 	opposite of what i want. for some reason the following hacks let
 	control keys through. i don't know why, but it's what i want, so i'm
 	not sweating it.]]
-local key_meta = getmetatable(keys)
+
+local modes =
+{
+	command =
+	{
+		['cb']	=	function()
+					buffer:page_up()
+					if 3 >= buffer.lines_on_screen then
+						buffer:line_scroll_down()
+						buffer:line_scroll_donw()
+					end
+				end,
+		--todo move caret into view
+		['cd']	=	function()
+					local half = math.floor(buffer.lines_on_screen/2)
+					half = half or 1
+					buffer.first_visible_line =
+						buffer.first_visible_line + half
+				end,
+		['h']	=	buffer.char_left,
+		['j']	=	buffer.line_down,
+		['k']	=	buffer.line_up,
+		['l']	=	buffer.char_right,
+		['i']	=	insert_mode,
+	},
+	insert =
+	{
+		['c@']	=	unimplemented,
+		['cd']	=	function()
+					buffer:back_tab()
+				end
+	}
+}
+
 local tavi_meta = {}
 setmetatable(keys,tavi_meta)
 
 function tavi_meta.__index(table, key)
+	--just ignore the builtin mode mechanism
 	if 'MODE' == key then
 		return nil
-	elseif mode == 'command' then
-		return command[key] or update_ui
+	elseif 'insert' == mode then
+		return modes[mode][key] --don't filter out extra stuff in insert mode
 	else
-		return key_meta and key_meta.__index and key_meta.__index(table,key)
+		return modes[mode][key] or update_ui
 	end
 end
 
-events.connect(events.UPDATE_UI, update_ui)
+--control sequences, just forward as required
+keys['c@']	=	function() modes[mode]['c@']() end
+keys['cb']	=	function() modes[mode]['cb']() end
+keys['cd']	=	function() modes[mode]['cd']() end
 
-command_mode()
+events.connect(events.UPDATE_UI, update_ui)
 
 return M
